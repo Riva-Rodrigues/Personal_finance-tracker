396b0b1f91eab38668b12dbbc639a60638bdfe84
Riva <rodriguesriva1130@gmail.com>
2024-09-29 09:00:30 +0530

    updated utils

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { urlencoded } from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
@@ -13,5 +13,13 @@ app.use(cors({
 app.use(express.json({
     limit:"16kb "
 }))
+
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb"
+}))
+
+app.use(express.static("public"))
+app.use(cookieParser())
 
 export {app}
